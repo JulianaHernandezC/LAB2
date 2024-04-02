c73d5d0e610666ad95d3d5a1b82a149fd965ce6c
JulianaHernandezC <131790646+JulianaHernandezC@users.noreply.github.com>
2024-04-02 19:22:20 -0300

    Update scriptShell3.sh

--- Ejercicio3/scriptShell3.sh
+++ Ejercicio3/scriptShell3.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 UMBRAL=10  # Umbral de espacio en disco en porcentaje
+
 # Obtener información sobre el espacio en disco
 ESPACIO=$(df -h / | awk 'NR==2 {print $5}' | sed 's/%//')
 
@@ -10,6 +11,4 @@ if [ $ESPACIO -gt $UMBRAL ]; then
 umbral del $UMBRAL%."
 else
     echo "El espacio en disco disponible es del $ESPACIO%, por debajo del umbral del $UMBRAL%."
-
-    # Aquí puedes agregar código para enviar una notificación al usuario,  por ejemplo, mediante un correo electrónico o un mensaje.
 fi
