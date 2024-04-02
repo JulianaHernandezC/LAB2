8b4a381fd83a94bc767b3553be2d4737052d337d
JulianaHernandezC <131790646+JulianaHernandezC@users.noreply.github.com>
2024-04-02 19:21:50 -0300

    Update scriptShell3.sh

--- Ejercicio3/scriptShell3.sh
+++ Ejercicio3/scriptShell3.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-# UMBRAL=1000  # Umbral de espacio disponible en MB
-
-
-# ESPACIO=$(df -P / | awk 'NR==2 {print $4}' | sed 's/[^0-9]*//g') # Obtener el espacio disponible en disco en MB
-
-
-# if [ $ESPACIO -lt $UMBRAL ]; then # Verificar si el espacio disponible es menor que el umbral
-#     echo "El espacio disponible en disco es inferior a $UMBRAL MB. Actualmente hay $ESPACIO MB."
-# else
-#     echo "El espacio disponible en disco es superior a $UMBRAL MB. Actualmente hay $ESPACIO MB."
-#     # cron
-# fi
-
 UMBRAL=10  # Umbral de espacio en disco en porcentaje
 # Obtener información sobre el espacio en disco
 ESPACIO=$(df -h / | awk 'NR==2 {print $5}' | sed 's/%//')
