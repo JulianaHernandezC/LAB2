688b56e4dceebd22af683b46006a8e42b8937163
JulianaHernandezC <131790646+JulianaHernandezC@users.noreply.github.com>
2024-04-12 08:46:25 -0300

    Update scriptShell3.sh

--- Ejercicio3/scriptShell3.sh
+++ Ejercicio3/scriptShell3.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+#Configuracion del cron para que el script sea ejecutado cada 5 minutos.
+*/5 * * * * /Users/julianahernandez/Desktop/Universidad/Primer Semstre 2024/Sistemas Operativos/LAB2.1/Ejercicio3/scriptShell3.sh
+
 UMBRAL=10  # Umbral de espacio en disco en porcentaje
 
-# Obtener información sobre el espacio en disco
+# Para obtener información sobre el espacio en disco
 ESPACIO=$(df -h / | awk 'NR==2 {print $5}' | sed 's/%//')
 
-# Verificar si el espacio disponible es menor que el umbral
+# Para verificar si el espacio disponible es menor que el umbral
 if [ $ESPACIO -gt $UMBRAL ]; then
     echo "El espacio en disco disponible es del $ESPACIO%, por encima del 
 umbral del $UMBRAL%."
